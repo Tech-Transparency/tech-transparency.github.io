cd49ce1014e4578373a42072af8e26f2bbcde61e
Neel Mehta <neelmehta96@gmail.com>
2018-08-29 20:56:30 -0700

    navbar changes colors on scroll

---
+++ assets/js/homepage.js
@@ -0,0 +1,17 @@
+var waypoint = new Waypoint({
+  element: $('#section-after-hero'),
+  handler: function(direction) {
+    // toggle navbar's transparency based on how far down the page you are
+    var navbar = $('#main-navbar');
+    if (direction === "down") {
+      // scrolling down past the hero, turn the navbar white
+      navbar.removeClass("bg-transparent")
+        .addClass("bg-light");
+    }
+    else if (direction === "up") {
+      // scrolling up to the hero, turn navbar transparent
+      navbar.removeClass("bg-light")
+        .addClass("bg-transparent");
+    }
+  }
+});
